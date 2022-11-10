eb815b7fec55c51074a4634d77a0200ea7828921
usama-365 <usama-365@protonmail.com>
2022-11-10 13:57:09 +0500

    Added fonts

--- App.js
+++ App.js
@@ -3,7 +3,17 @@ import {RestaurantScreen} from "./src/features/restaurants/screens/restaurant.sc
 import {ThemeProvider} from "styled-components/native";
 import {theme} from "./src/infrastructure/theme";
 
+import {Oswald_400Regular, useFonts as useOswald} from "@expo-google-fonts/oswald";
+import {Lato_400Regular, useFonts as useLato} from "@expo-google-fonts/lato";
+
 export default function App() {
+    // Loading fonts
+    const [oswaldLoaded] = useOswald({Oswald_400Regular});
+    const [latoLoaded] = useLato({Lato_400Regular});
+    // Validating fonts
+    if (!oswaldLoaded || !latoLoaded) {
+        return null;
+    }
     return (
         <>
             <ThemeProvider theme={theme}>
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,4 +1,4 @@
-import {Text} from "react-native";
+import {Text, View} from "react-native";
 import {Card} from "react-native-paper";
 import styled from "styled-components/native";
 
@@ -13,8 +13,18 @@ const RestaurantCover = styled(Card.Cover)`
 `;
 
 const Title = styled(Text)`
-  padding: ${props => props.theme.space[3]};
+  font-family: ${props => props.theme.fonts.heading};
+  font-size: ${props => props.theme.fontSizes.body};
   color: ${props => props.theme.colors.ui.primary};
+`;
+
+const Info = styled(View)`
+  padding: ${props => props.theme.space[3]};
+`;
+
+const Address = styled(Text)`
+  font-size: ${props => props.theme.fontSizes.caption};
+  font-family: ${props => props.theme.fonts.body};
 `;
 
 // Component
@@ -28,7 +38,10 @@ export const RestaurantInfoCard = function ({restaurant = {}}) {
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCover key={name} source={{uri: photos[0]}}></RestaurantCover>
-            <Title>{name}</Title>
+            <Info>
+                <Title>{name}</Title>
+                <Address>{address}</Address>
+            </Info>
         </RestaurantCard>
     );
 };
