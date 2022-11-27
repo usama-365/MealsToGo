fe55c7804d6962139852887e4638c3473b77b91d
usama-365 <usama-365@protonmail.com>
2022-11-27 12:05:41 +0500

    Added Address and Rating SVG

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,9 @@
 import {Text, View} from "react-native";
 import {Card} from "react-native-paper";
 import styled from "styled-components/native";
+import {SvgXml} from "react-native-svg";
+import star from "../../../../assets/star";
+
 
 // Styled components
 const RestaurantCard = styled(Card)`
@@ -27,6 +30,13 @@ const Address = styled(Text)`
   font-family: ${props => props.theme.fonts.body};
 `;
 
+const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${props => props.theme.space[2]};
+  padding-bottom: ${props => props.theme.space[2]};
+
+`;
+
 // Component
 export const RestaurantInfoCard = function ({restaurant = {}}) {
     const {
@@ -34,12 +44,15 @@ export const RestaurantInfoCard = function ({restaurant = {}}) {
             "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         ], address = "100 some random street", isOpenNow = true, rating = 4, isClosedTemporarily
     } = restaurant;
-
+    const ratingArray = Array(Math.round(rating)).fill(0);
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCover key={name} source={{uri: photos[0]}}></RestaurantCover>
             <Info>
                 <Title>{name}</Title>
+                <Rating>
+                    {ratingArray.map(_ => <SvgXml width={20} height={20} xml={star}/>)}
+                </Rating>
                 <Address>{address}</Address>
             </Info>
         </RestaurantCard>
