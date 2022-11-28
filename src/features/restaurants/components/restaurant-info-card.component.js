c1cefaa0105aea04de3405b59de884818a7f23ba
usama-365 <usama-365@protonmail.com>
2022-11-28 17:30:17 +0500

    Seperated the styled components from restaurant info card

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,51 +1,18 @@
-import {Image, View} from "react-native";
-import {Card} from "react-native-paper";
-import styled from "styled-components/native";
 import {SvgXml} from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import {Spacer} from "../../../components/spacer/spacer.component";
 import {Text} from "../../../components/typography/text.component";
-import {Icon} from "./restaurant-info-card.styles";
-
-
-// Styled components
-const RestaurantCard = styled(Card)`
-  background-color: ${props => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCover = styled(Card.Cover)`
-  background-color: white;
-  padding: ${props => props.theme.space[3]};
-`;
-
-const Info = styled(View)`
-  padding: ${props => props.theme.space[3]};
-`;
-
-const Address = styled(Text)`
-  font-size: ${props => props.theme.fontSizes.caption};
-  font-family: ${props => props.theme.fonts.body};
-`;
-
-const Rating = styled(View)`
-  flex-direction: row;
-  padding-top: ${props => props.theme.space[2]};
-  padding-bottom: ${props => props.theme.space[2]};
-  justify-content: space-between;
-`;
-
-const Section = styled(View)`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled(View)`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
+import {
+    Address,
+    Icon,
+    Info,
+    Rating,
+    RestaurantCard,
+    RestaurantCover,
+    Section,
+    SectionEnd
+} from "./restaurant-info-card.styles";
 
 
 // Component
@@ -73,9 +40,9 @@ export const RestaurantInfoCard = function ({restaurant = {}}) {
                     </Rating>
                     <SectionEnd>
                         {isClosedTemporarily && <Text variant="error">CLOSED TEMPORARILY</Text>}
-                        <Spacer position="left" size='large'></Spacer>
+                        <Spacer position="left" size="large"></Spacer>
                         {isOpenNow && <SvgXml width={20} height={20} xml={open}></SvgXml>}
-                        <Spacer position="left" size='large'></Spacer>
+                        <Spacer position="left" size="large"></Spacer>
                         <Icon source={{uri: icon}}></Icon>
                     </SectionEnd>
                 </Section>
--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -1,5 +1,44 @@
 import styled from "styled-components/native";
-import {Image} from "react-native";
+import {Image, View} from "react-native";
+import {Card} from "react-native-paper";
+import {Text} from "../../../components/typography/text.component";
+
+// Styled components
+export const RestaurantCard = styled(Card)`
+  background-color: ${props => props.theme.colors.bg.primary};
+`;
+
+export const RestaurantCover = styled(Card.Cover)`
+  background-color: white;
+  padding: ${props => props.theme.space[3]};
+`;
+
+export const Info = styled(View)`
+  padding: ${props => props.theme.space[3]};
+`;
+
+export const Address = styled(Text)`
+  font-size: ${props => props.theme.fontSizes.caption};
+  font-family: ${props => props.theme.fonts.body};
+`;
+
+export const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${props => props.theme.space[2]};
+  padding-bottom: ${props => props.theme.space[2]};
+  justify-content: space-between;
+`;
+
+export const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
 
 export const Icon = styled(Image)`
   width: 15px;
