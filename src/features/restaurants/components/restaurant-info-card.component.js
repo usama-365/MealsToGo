6b41a31c3278f898c6f7acc846e6eeb150b03420
usama-365 <usama-365@protonmail.com>
2022-11-28 17:27:13 +0500

    Integrated a typography design system

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,10 +1,12 @@
-import {Image, Text, View} from "react-native";
+import {Image, View} from "react-native";
 import {Card} from "react-native-paper";
 import styled from "styled-components/native";
 import {SvgXml} from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 import {Spacer} from "../../../components/spacer/spacer.component";
+import {Text} from "../../../components/typography/text.component";
+import {Icon} from "./restaurant-info-card.styles";
 
 
 // Styled components
@@ -15,12 +17,6 @@ const RestaurantCard = styled(Card)`
 const RestaurantCover = styled(Card.Cover)`
   background-color: white;
   padding: ${props => props.theme.space[3]};
-`;
-
-const Title = styled(Text)`
-  font-family: ${props => props.theme.fonts.heading};
-  font-size: ${props => props.theme.fontSizes.body};
-  color: ${props => props.theme.colors.ui.primary};
 `;
 
 const Info = styled(View)`
@@ -50,6 +46,8 @@ const SectionEnd = styled(View)`
   justify-content: flex-end;
 `;
 
+
+
 // Component
 export const RestaurantInfoCard = function ({restaurant = {}}) {
     const {
@@ -68,17 +66,17 @@ export const RestaurantInfoCard = function ({restaurant = {}}) {
         <RestaurantCard elevation={5}>
             <RestaurantCover key={name} source={{uri: photos[0]}}></RestaurantCover>
             <Info>
-                <Title>{name}</Title>
+                <Text variant="label">{name}</Text>
                 <Section>
                     <Rating>
                         {ratingArray.map(_ => <SvgXml width={20} height={20} xml={star}/>)}
                     </Rating>
                     <SectionEnd>
-                        {isClosedTemporarily && <Text variant="label" style={{color: "red"}}>CLOSED TEMPORARILY</Text>}
+                        {isClosedTemporarily && <Text variant="error">CLOSED TEMPORARILY</Text>}
                         <Spacer position="left" size='large'></Spacer>
                         {isOpenNow && <SvgXml width={20} height={20} xml={open}></SvgXml>}
-                        <Spacer position="left" size='small'></Spacer>
-                        <Image style={{width: 15, height: 15}} source={{uri: icon}}></Image>
+                        <Spacer position="left" size='large'></Spacer>
+                        <Icon source={{uri: icon}}></Icon>
                     </SectionEnd>
                 </Section>
                 <Address>{address}</Address>
---
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+import {Image} from "react-native";
+
+export const Icon = styled(Image)`
+  width: 15px;
+  height: 15px;
+`;
