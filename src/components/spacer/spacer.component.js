a30ea5566611c6e801ebbb1c7f4cd15f7fe969f3
usama-365 <usama-365@protonmail.com>
2022-11-28 16:51:12 +0500

    Added a spacing system

---
+++ src/components/spacer/spacer.component.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {View} from "react-native";
+import styled, {useTheme} from "styled-components/native";
+
+const sizeVariant = {
+    small: 1,
+    medium: 2,
+    large: 3
+};
+
+const positionVariant = {
+    top: "marginTop",
+    left: "marginLeft",
+    right: "marginRight",
+    bottom: "marginRight"
+};
+
+const getStyleString = function (position, size, theme) {
+    const propertyName = positionVariant[position];
+    const propertyValue = theme.space[sizeVariant[size]];
+    console.log(`${propertyName}:${propertyValue};`);
+    return `${propertyName}:${propertyValue};`;
+};
+
+const SpacerView = styled(View)`
+  ${({variant}) => variant};
+`;
+
+export const Spacer = function ({position, size, children}) {
+    const theme = useTheme();
+    const variant = getStyleString(position, size, theme);
+    return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
+Spacer.defaultProps = {
+    size: "small",
+    position: "top"
+};
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -4,6 +4,7 @@ import styled from "styled-components/native";
 import {SvgXml} from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
+import {Spacer} from "../../../components/spacer/spacer.component";
 
 
 // Styled components
@@ -52,9 +53,15 @@ const SectionEnd = styled(View)`
 // Component
 export const RestaurantInfoCard = function ({restaurant = {}}) {
     const {
-        name = "Some Restaurant", icon= "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png", photos = [
+        name = "Some Restaurant",
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+        photos = [
             "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        ], address = "100 some random street", isOpenNow = true, rating = 4, isClosedTemporarily = true
+        ],
+        address = "100 some random street",
+        isOpenNow = true,
+        rating = 4,
+        isClosedTemporarily = true
     } = restaurant;
     const ratingArray = Array(Math.round(rating)).fill(0);
     return (
@@ -67,11 +74,11 @@ export const RestaurantInfoCard = function ({restaurant = {}}) {
                         {ratingArray.map(_ => <SvgXml width={20} height={20} xml={star}/>)}
                     </Rating>
                     <SectionEnd>
-                        {isClosedTemporarily && <Text variant="label" style={{color: 'red'}}>CLOSED TEMPORARILY</Text>}
-                        <View style={{paddingLeft: 16}} />
+                        {isClosedTemporarily && <Text variant="label" style={{color: "red"}}>CLOSED TEMPORARILY</Text>}
+                        <Spacer position="left" size='large'></Spacer>
                         {isOpenNow && <SvgXml width={20} height={20} xml={open}></SvgXml>}
-                        <View style={{paddingLeft: 16}} />
-                            <Image style={{width: 15, height: 15}} source={{uri: icon}}></Image>
+                        <Spacer position="left" size='small'></Spacer>
+                        <Image style={{width: 15, height: 15}} source={{uri: icon}}></Image>
                     </SectionEnd>
                 </Section>
                 <Address>{address}</Address>
