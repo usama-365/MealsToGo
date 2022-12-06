514c55d93b29f07a3789fd8a63021dff07d7ca3d
usama-365 <usama-365@protonmail.com>
2022-12-06 16:51:53 +0500

    Added activity indicator

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import {FlatList, View} from "react-native";
-import {Searchbar} from "react-native-paper";
+import {ActivityIndicator, Colors, Searchbar} from "react-native-paper";
 import {RestaurantInfoCard} from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 import {RestaurantsContext} from "../../../services/restaurants/mock/restaurants.context";
@@ -9,12 +9,17 @@ const SearchContainer = styled(View)`
   padding: ${props => props.theme.space[3]};
 `;
 
-
 const RestaurantList = styled(FlatList).attrs({
     contentContainerStyle: {
         padding: 16
     }
 })``;
+
+const CenteredView = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
 
 export const RestaurantScreen = function () {
     const {restaurants, isLoading, error} = useContext(RestaurantsContext);
@@ -23,11 +28,14 @@ export const RestaurantScreen = function () {
             <SearchContainer>
                 <Searchbar></Searchbar>
             </SearchContainer>
-            <RestaurantList
-                data={restaurants}
-                renderItem={(item) => <RestaurantInfoCard restaurant={item} />}
-                keyExtractor={item => item.index}
-            />
+            {isLoading ? (
+                <CenteredView>
+                    <ActivityIndicator style={{ marginLeft: -25 }} color={Colors.blue300} size={50} animating={true} />
+                </CenteredView>
+            ) : (
+                <RestaurantList data={restaurants} renderItem={(item) => <RestaurantInfoCard restaurant={item} />} keyExtractor={item => item.index}/>
+            )}
+            
         </>
     );
 }
