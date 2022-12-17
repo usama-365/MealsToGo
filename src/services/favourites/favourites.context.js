913b693d622ca7844963eb0f6ef5491898e885b0
usama-365 <usama-365@protonmail.com>
2022-12-17 20:18:30 +0500

    Favourites are now saved to local storage

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,11 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = function ({ children }) {
     const [favourites, setFavourites] = useState([]);
+
+    const saveFavourites = async function (value) {
+        try {
+            const jsonValue = JSON.stringify(value);
+            await AsyncStorage.setItem("@favourites", jsonValue);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const loadFavourites = async function () {
+        try {
+            const value = await AsyncStorage.getItem("@favourites");
+            if (value) setFavourites(JSON.parse(value));
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     const add = (restaurant) => setFavourites([...favourites, restaurant]);
     const remove = (restaurant) => setFavourites(favourites.filter((x) => x.placeId !== restaurant.placeId));
+
+    useEffect(() => { loadFavourites(); }, []);
+    useEffect(() => { saveFavourites(favourites); }, [favourites]);
+
     return (
         <FavouritesContext.Provider value={{ favourites, addToFavourites: add, removeFromFavourites: remove }}>
             {children}
