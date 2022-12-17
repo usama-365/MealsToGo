705f1d5b55d500255dcc5934cc21df6a87ff2018
usama-365 <usama-365@protonmail.com>
2022-12-17 15:39:35 +0500

    Added a search component on map screen

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,6 +1,18 @@
-import React from "react"
-import MapView from "react-native-maps"
+import React from "react";
+import MapView from "react-native-maps";
+import styled from "styled-components";
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+    height: 100%;
+    width: 100%;
+`;
 
 export const MapScreen = function () {
-    return (<MapView style={{ height: '100%', width: '100%'}} />);
+    return (
+        <>
+            <Search />
+            <Map />
+        </>
+    );
 }
