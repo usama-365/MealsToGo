5e416769cf6a95afff42703d7e79f23f775e7d71
usama-365 <usama-365@protonmail.com>
2022-12-19 13:25:33 +0500

    Created account screen buttons

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import {ImageBackground, View} from "react-native";
+import {Button} from "react-native-paper";
+import {colors} from "../../../infrastructure/theme/colors";
 
 export const AccountBackground = styled(ImageBackground).attrs({
     source: require('../../../../assets/home-bg.jpg')
@@ -15,4 +17,16 @@ export const AccountCover = styled(View)`
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.3);
+`;
+
+export const AccountContainer = styled(View)`
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: ${props => props.theme.space[4]};
+  margin-top: ${props => props.theme.space[2]};
+`;
+
+export const AuthButton = styled(Button).attrs({
+    color: colors.brand.primary
+})`
+    padding: ${props => props.theme.space[2]};
 `;
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,9 +1,15 @@
-import {AccountBackground, AccountCover} from "../components/account.styles";
+import {AccountBackground, AccountContainer, AccountCover, AuthButton} from "../components/account.styles";
+import {Spacer} from "../../../components/spacer/spacer.component";
 
-export const AccountScreen = function () {
+export const AccountScreen = function ({navigation}) {
     return (
         <AccountBackground>
             <AccountCover/>
+            <AccountContainer>
+                <AuthButton icon="lock-open-outline" mode="contained" onPress={() => navigation.navigate("Login")}>Login</AuthButton>
+                <Spacer position="top" size="large"/>
+                <AuthButton icon="lock-open-outline" mode="contained" onPress={() => navigation.navigate("Register")}>Register</AuthButton>
+            </AccountContainer>
         </AccountBackground>
     );
 }
