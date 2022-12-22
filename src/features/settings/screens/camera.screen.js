36fa83dcb3f282c33fdc1564b57904c991c0ea7a
usama-365 <usama-365@protonmail.com>
2022-12-22 14:00:06 +0500

    Added camera functionality

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,5 +1,63 @@
-import React from "react";
+import React, {useContext, useRef} from "react";
+import {Camera, CameraType} from "expo-camera";
+import styled from "styled-components";
+import {SafeArea} from "../../../components/utils/safe-area.component";
+import {Button} from "react-native-paper";
+import {Text} from "../../../components/typography/text.component";
+import {TouchableOpacity, View} from "react-native";
+import {AuthenticationContext} from "../../../services/authentication/authentication.context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const CameraScreen = function () {
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+`;
 
+const InnerSnap = styled(View)`
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+`;
+
+const CenteredSafeArea = styled(SafeArea)`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CameraScreen = function ({navigation}) {
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+    const cameraRef = useRef();
+    const {user} = useContext(AuthenticationContext);
+
+    const snap = async function () {
+        if (cameraRef) {
+            console.log(cameraRef.current);
+            const photo = await cameraRef.current.takePictureAsync();
+            console.log(photo);
+            await AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+            navigation.goBack();
+        }
+    };
+
+    if (!permission) {
+        // Camera permissions are still loading
+        return null;
+    } else if (!permission.granted) {
+        // Camera permissions are not granted yet
+        return (
+            <CenteredSafeArea>
+                <Text style={{textAlign: "center"}}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission">Grant Permission</Button>
+            </CenteredSafeArea>
+        );
+    } else return (
+        <ProfileCamera type={CameraType.front} ref={(camera) => {
+            cameraRef.current = camera;
+        }}>
+            <TouchableOpacity onPress={snap}>
+                <InnerSnap/>
+            </TouchableOpacity>
+        </ProfileCamera>
+    );
 };
--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,11 +1,13 @@
 import {AuthenticationContext} from "../../../services/authentication/authentication.context";
 import {SafeArea} from "../../../components/utils/safe-area.component";
 import {Avatar, List} from "react-native-paper";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import styled from "styled-components";
 import {TouchableOpacity, View} from "react-native";
 import {Spacer} from "../../../components/spacer/spacer.component";
 import {Text} from "../../../components/typography/text.component";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {useFocusEffect} from "@react-navigation/native";
 
 const SettingsItem = styled(List.Item)`
   padding: ${props => props.theme.space[3]};
@@ -17,12 +19,18 @@ const AvatarContainer = styled(View)`
 
 export const SettingsScreen = ({navigation}) => {
     const {onLogout, user} = useContext(AuthenticationContext);
+    const [photo, setPhoto] = useState(null);
+    const getProfilePicture = async function (user) {
+        const photoURI = await AsyncStorage.getItem(`${user.uid}-photo`);
+        setPhoto(photoURI);
+    }
+    useFocusEffect(() => {getProfilePicture(user);});
     return (
         <SafeArea>
             <Spacer position="top" size="large"/>
             <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
             <AvatarContainer>
-                <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD"/>
+                {photo ? <Avatar.Image source={photo} size={180}/> : <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD"/>}
                 <Spacer position="top" size="large">
                     <Text variant="label">{user.email}</Text>
                 </Spacer>
